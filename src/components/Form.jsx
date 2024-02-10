e8f18a89dcee5026ce68963985852dc9644e6756
HitalloFeitosa <hitallo.feitosa@oficialfarma.com.br>
2024-02-09 21:55:37 -0300

    feat: Add form styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,17 +10,19 @@ export default function Form() {
             <form onSubmit={handleSubmit((data) => {
                 console.log(data)
             })}>
-                <h2>Realize seu login</h2>        
-                <input type="text" {...register("firstName", { required: 'Campo obrigatório!' })} placeholder="Nome de usuário" />
-                <p>{errors.firstName?.message}</p>
+                <div className="form-info">
+                    <h2>Faça seu Login</h2>        
+                    <input type="text" {...register("firstName", { required: 'Campo obrigatório!' })} placeholder="Nome de usuário" />
+                    <p>{errors.firstName?.message}</p>
 
-                <input type="text" {...register("password", { required: 'Campo obrigatório!', minLength: {
-                    value: 6,
-                    message: "Insira o minimo de 6 caracteres" 
-                }})} placeholder="Senha" />
-                <p>{errors.password?.message}</p>
+                    <input type="password" {...register("password", { required: 'Campo obrigatório!', minLength: {
+                        value: 6,
+                        message: "Insira o minimo de 6 caracteres" 
+                    }})} placeholder="Senha" />
+                    <p>{errors.password?.message}</p>
 
-                <input type="submit" />
+                    <input type="submit" className="btn-submit-form" />
+                </div>
             </form> 
         </>
     )
