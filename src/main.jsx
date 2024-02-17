aa85d1c3bb11eaeff2dd87c4392418e7601b4aa9
HitalloFeitosa <hitallo.feitosa@oficialfarma.com.br>
2024-02-16 21:05:46 -0300

    feat: change form

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import './styles/variables.scss'
 import './styles/form.scss'
-import './styles/_variables.scss'
+import './styles/sucesso.scss'
+import AppRoutes from './routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppRoutes />
   </React.StrictMode>,
 )
